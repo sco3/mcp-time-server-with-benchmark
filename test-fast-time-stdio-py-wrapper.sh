50eb19a5a96f28803490431a369bdadd014e35a9
Dima Zakharov <zakharov@ibm.com>
2026-01-29 14:49:23 +0000

    wrapper test

---
+++ test-fast-time-stdio-py-wrapper.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S bash
+
+set -ueo pipefail
+
+rm -f out.log
+
+EXE=(fast-time-server)
+
+INIT='{"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2024-11-05","capabilities":{},"clientInfo":{"name":"test-client","version":"1.0"}}}'
+NOTIFY='{"jsonrpc":"2.0","id":2, "method":"notifications/initialized"}'
+LIST='{"jsonrpc":"2.0","id":3,"method":"tools/list"}'
+
+CALL='{"jsonrpc": "2.0","id": 4,"method": "tools/call","params": {"name": "get_system_time","arguments": {}}}'
+TEMPLATE='{"jsonrpc": "2.0","id": REPLACE_ID,"method": "tools/call","params": {"name": "get_system_time","arguments": {}}}'
+(
+	echo "$INIT"
+	sleep 0.2
+	echo "$NOTIFY"
+	sleep 0.2
+	echo "$LIST"
+	sleep 0.2
+	for i in {4..1000}; do
+		echo "${TEMPLATE/REPLACE_ID/$i}"
+	done
+) | "${EXE[@]}"
